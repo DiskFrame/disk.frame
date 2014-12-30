692447318e5469ee8c1abdfa404cf2ae26780108
rstudio <rstudio@example.com>
2014-12-30 11:56:41 +0000

    minor

--- R/csv.to.disk.frame.r
+++ R/csv.to.disk.frame.r
@@ -1,15 +1,24 @@
-
-csv.to.disk.frame <- function(infile, outpath, ..., mem.prop = 0.1) {
-  require(data.table)
-  #browser()
+# depends on data.table
+#require(data.table)
+csv.to.disk.frame <- function(infile, outpath, ..., mem.prop = 0.1) {  
   if (length(dir(outpath)) == 0) {
     dir.create(outpath)
   }
+  
+  Sys.info()["sysname"]
   mb = 1048576 # number of bytes in megabyte
-  memlimit = memory.limit() * mb # the memory limit
+  
+  if(Sys.info()["sysname"] == "Windows") {
+    memlimit = memory.limit() * mb # the memory limit
+  } else {
+    # assume 4 gigs
+    memlimit = mb * 1024 * 4 
+  }
+    
   
   # the infile size
-  infile.size <- file.size(infile)
+  # do NOT use file.size this function does not work on digitial ocean
+  infile.size <- file.info(infile)$size
     
   # set a connection to the file so the file can be read nrows at a time
   f <- file(infile)
@@ -25,7 +34,8 @@ csv.to.disk.frame <- function(infile, outpath, ..., mem.prop = 0.1) {
   # read 50 lines to try and have a first guess at the size of the data
   data <-read.csv(f,nrows=50, ...)
   var.names = names(data)    
-  provlinesize = pryr::object_size(data)/50  
+  #provlinesize = pryr::object_size(data)/50  
+  provlinesize = object.size(data)/50  
   close(f, blocking =TRUE) # now that I have determined n
   
   # the theory is the more you read in the first round the less likely your data format to be wrong
@@ -45,15 +55,15 @@ csv.to.disk.frame <- function(infile, outpath, ..., mem.prop = 0.1) {
   print(paste0("reading in ", n, " rows as chunk number: 1"))    
   print(system.time(data <-data.table(read.csv(f, nrows=n, ...))))
   
-  # write one row to disk to get an idea of file size
+  # write some rows to disk to get an idea of file size
   tf <- tempfile()
-  write.csv(file = tf, data[1:1024,] , row.names = FALSE, col.names= FALSE)
+  write.csv(data[1:1024,], file = tf, row.names = FALSE, col.names = FALSE)
   
   # the approximate total file size fpr the first chunk
-  tfs <- file.size(tf)/1024 * n
+  tfs <- file.info(tf)$size/1024 * n
     
   # a more realistic estimate of line size
-  provlinesize = pryr::object_size(data)  / n 
+  provlinesize = object.size(data)  / n 
   
   #setting up disk.frame folder
 
@@ -75,19 +85,19 @@ csv.to.disk.frame <- function(infile, outpath, ..., mem.prop = 0.1) {
     
     # write a file to disk
     tf <- tempfile()
-    write.csv(file = tf, data[1:1024,] , row.names = FALSE, col.names = FALSE)
+    write.csv(data[1:1024,], file = tf, row.names = FALSE, col.names = FALSE)
     
     # the total file size in bytes including header
-    tfs <- file.size(tf)/1024*n + tfs
+    tfs <- file.info(tf)$size/1024*n + tfs
     
     
-    provlinesize <- (pryr::object_size(data)  + totreadn*provlinesize) / (totreadn + n) #95% CI of line size
+    provlinesize <- (object.size(data)  + totreadn*provlinesize) / (totreadn + n) #95% CI of line size
     totreadn <- totreadn + n        
   }
   close(f)
   if (nrow(data) != 0 ) {
     n = nrow(data)
     print(paste0("but only ", n, " read. writing chunk number: ", chunk))
-    #print(system.time(save(data, file = file.path(outpath,paste0(chunk,".disk.frame")))))
+    print(system.time(save(data, file = file.path(outpath,paste0(chunk,".disk.frame")))))
   }
 }
