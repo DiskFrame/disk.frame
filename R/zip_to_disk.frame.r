30bca285272d5b07dc42db84caae1a7f5592379f
susabi <zhuojia.dai@gmail.com>
2018-01-08 23:38:57 +1100

    write to .fst.tmp and then rename to .fst

--- R/zip_to_disk.frame.r
+++ R/zip_to_disk.frame.r
@@ -14,7 +14,7 @@ zip_to_disk.frame = function(zipfile, outpath, ... , replace = F, validation.che
       print(fn)
       out_fst_file = file.path(outpath, paste0(fn,".fst"))
       
-      if(file.exists(out_fst_file)) {
+      if(!replace & file.exists(out_fst_file)) {
         print("output already exists")
         return(NULL)
       }
@@ -22,8 +22,9 @@ zip_to_disk.frame = function(zipfile, outpath, ... , replace = F, validation.che
       pt = proc.time()
       unzip(zipfile, files = fn, exdir = "fm_perf")
       print(paste0("unzip: ", timetaken(pt))); pt = proc.time()
-      write_fst(fread(file.path("fm_perf", fn)), out_fst_file,100)
+      write_fst(fread(file.path("fm_perf", fn)), paste0(out_fst_file,".tmp"),100)
       print(paste0("read: ", timetaken(pt)))
+      file.rename(paste0(out_fst_file,".tmp"), out_fst_file)
       unlink(file.path("fm_perf", fn))
       gc()
     }))
@@ -40,8 +41,9 @@ zip_to_disk.frame = function(zipfile, outpath, ... , replace = F, validation.che
       pt = proc.time()
       unzip(zipfile, files = fn, exdir = "fm_perf")
       print(paste0("unzip: ", timetaken(pt))); pt = proc.time()
-      write_fst(fread(file.path("fm_perf", fn)), out_fst_file,100)
+      write_fst(fread(file.path("fm_perf", fn)), paste0(out_fst_file,".tmp"),100)
       print(paste0("read: ", timetaken(pt)))
+      file.rename(paste0(out_fst_file,".tmp"), out_fst_file)
       unlink(file.path("fm_perf", fn))
       gc()
     }))
