9041e200f2d1be69af02f889f4880b33478c8b36
susabi <zhuojia.dai@gmail.com>
2018-08-15 14:30:11 +1000

    minor + made convert2disk.frame into a function

--- R/conversion_r.r
+++ R/conversion_r.r
@@ -1,5 +1,4 @@
-function() {
-  library(data.table)
+library(data.table)
 library(fst)
 library(future)
 library(stringr)
@@ -34,40 +33,49 @@ extracting = rep(F, nchunks)
 # function to convert some chunks at a time
 sas2csv <- function(chunks) {  
   dir.create(file.path("outcsv",as.character(chunks)))
-  options = sprintf("-o outcsv/%s/ -c -n %d -k %s -m", chunks, nchunks, paste(chunks,collapse = " "))  
+  options = sprintf("-o outcsv/%s/ -c -n %d -k %s -m", chunks, nchunks, paste(chunks,collapse = " "))
   cmd = paste(sas2csvpath, sasfile, options)
   system(cmd)
 }
 
-dir.create(out_fst_path)
-
-while(!all(ready)) {
-  done1 = F
-  extracting_jobs = F
-  for(w in which(!ready)) {
-    incsv = file.path("outcsv", w-1,paste0("_", w-1,".csv"))
-    if(file.exists(incsv)) {
-      done1 = T
-      ready[w] = T
-      ok %<-% {
-        write_fst(fread(incsv), file.path(out_fst_path, paste0(w-1,".fst")), 100)
-        #unlink(incsv)
-        file.remove(incsv)
-        gc()
+# the function that orchestrates the conversion
+convert2disk.frame = function(inpath, outpath) {
+  nchunks = chunks
+  files = file.path(outpath, paste0(0:(nchunks-1),".fst"))
+  ready = rep(F, nchunks) | file.exists(files)
+  # ready = c(rep(T, 96), rep(F, 4))
+  extracting = rep(F, nchunks)
+  
+  if(!dir.exists(outpath)) dir.create(outpath)
+  
+  while(!all(ready)) {
+    done1 = F
+    extracting_jobs = F
+    for(w in which(!ready)) {
+      incsv = file.path("outcsv", w-1,paste0("_", w-1,".csv"))
+      if(file.exists(incsv)) {
+        done1 = T
+        ready[w] = T
+        ok %<-% {
+          write_fst(fread(incsv), file.path(outpath, paste0(w-1,".fst.tmp")), 70)
+          file.rename(file.path(outpath, paste0(w-1,".fst.tmp")), file.path(outpath, paste0(w-1,".fst")))
+          file.remove(incsv)
+          gc()
+        }
+        print(paste("converting: ", w-1, Sys.time()))
+      } else if (!extracting_jobs & !extracting[w]) {
+        done1 = T
+        extracting_jobs = T
+        extracting[w] <- T
+        ok %<-% {
+          sas2csv(inpath, w-1, chunks)
+        }
+        print(paste("extracting: ",w-1, Sys.time()))
       }
-      print(paste("converting: ", w-1, Sys.time()))
-    } else if (!extracting_jobs & !extracting[w]) {
-      done1 = T
-      extracting_jobs = T
-      extracting[w] <- T
-      ok %<-% {
-        sas2csv(w-1)
-      }
-      print(paste("extracting: ",w-1, Sys.time()))
+    }
+    if(!done1) {
+      print(paste0("didn't get any work :(", Sys.time()))
+      Sys.sleep(18)
     }
   }
-  if(!done1) {
-    print(paste0("didn't get any work :(", Sys.time()))
-  }
-}
 }
