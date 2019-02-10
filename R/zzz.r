971842a74883c37e7fd6600f77885ae80287fbae
Dai ZJ <zhuojia.dai@gmail.com>
2019-02-10 11:13:19 +0000

    got rid of NOTE with global variables

--- R/zzz.r
+++ R/zzz.r
@@ -7,6 +7,33 @@
   setup_disk.frame()
 }
 
+globalVariables(c(".",
+                  ".BY",
+                  ".N",
+                  ".SD",
+                  ".out.disk.frame.id",
+                  ":=",
+                  "N",
+                  "area",
+                  "chunk_id",
+                  "coltypes",
+                  "coltypes.x",
+                  "coltypes.y",
+                  "ctot",
+                  "existing_df",
+                  "feature_s",
+                  "h",
+                  "height",
+                  "incompatible_types",
+                  "lag_height",
+                  "new_chunk",
+                  "ok",
+                  "pathA",
+                  "pathB",
+                  "w",
+                  "xid",
+                  "yid"))
+
 #' @useDynLib disk.frame
 #' @importFrom Rcpp evalCpp
 #@exportPattern "^[[:alpha:]]+"
