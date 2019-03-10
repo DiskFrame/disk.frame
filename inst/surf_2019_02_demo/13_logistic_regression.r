dc24c87e05efafea9ecdf679f91c618f81891127
susabi <zhuojia.dai@gmail.com>
2019-03-10 15:52:52 +1100

    minor

--- inst/surf_2019_02_demo/13_logistic_regression.r
+++ inst/surf_2019_02_demo/13_logistic_regression.r
@@ -103,7 +103,40 @@ all_vars[, cscore_bin := cut(
   var1$bin[[2]] %>% setdiff(c(701,713))) %>% addNA]
 
 bp1 = all_vars[order(cscore_bin),.(sum(default_next_12m)/.N), cscore_bin]
-barplot(bp1$V1)
+barplot(
+  bp1$V1, 
+  names.arg = bp1[[1]], 
+  main = "cscore_b bin Default Rate"
+)
+
+setnames(bp1,"V1","def_rate")
+
+overall_df = all_vars[,sum(default_next_12m)/.N]
+
+bp1[,whole := overall_df]
+
+bp1[,woe := log((1-def_rate)/def_rate) - log((1-whole)/whole)]
+
+DT::datatable(bp1)
+
+glm(def_rate ~ I(-woe), data = bp1, family = binomial)
+
+all_vars[,csr_woe := bp1$woe[cscore_bin]]
+
+DT::datatable(all_vars[1:10,.(default_next_12m, cscore_bin, csr_woe)])
+
+glm(
+  default_next_12m ~ csr_woe,
+  family = binomial,
+  data = all_vars
+)
+
+View(two_var)
+
+
+
+
+
 
 var2 = res_all[[3]]
 all_vars[, ocltv_bin := cut(
@@ -111,7 +144,7 @@ all_vars[, ocltv_bin := cut(
   var2$bins[[2]]) %>% addNA]
 
 bp2 = all_vars[order(ocltv_bin),.(sum(default_next_12m)/.N), ocltv_bin]
-barplot(bp2$V1)
+barplot(bp2$V1, names.arg = bp2[[1]], main = "ocltv Orig LTV Default Rate")
 
 
 var2 = res_all[[9]]
@@ -120,9 +153,49 @@ all_vars[, dti_bin := cut(
   all_vars[,var2$feature, with = F][[1]],
   var2$bins[[2]]) %>% addNA]
 
-bp2 = all_vars[order(dti_bin),.(sum(default_next_12m)/.N), dti_bin]
-barplot(bp2$V1)
+dtrain <- xgboost::xgb.DMatrix(
+  label = two_var[,default_next_12m], 
+  data = as.matrix(two_var[,cscore_b]))
 
-m = glm(default_next_12m ~ cscore_bin + ocltv_bin + dti_bin, data=all_vars)
+m2 <- xgboost::xgboost(
+  data=dtrain, 
+  monotone_constraints = -1, # awesome!!!
+  nrounds = 1, # one tree ONLY
+  eta = 1, # learning rate 
+  
+  
+
+  objective = "binary:logitraw", 
+  tree_method="exact",
+  max_depth = 2,
+  base_score = two_var[,sum(default_next_12m)/.N]
+)
+
+xgboost::xgb.plot.tree(model=m2)
+
+plot(m2)
+
+bp2 = all_vars[order(dti_bin),.(sum(default_next_12m)/.N), dti_bin]
+barplot(bp2$V1, names.arg=bp2[[1]], main="dti Debt-to-income Ratio Default Rate")
+
+
+dt <- xgb.model.dt.tree(model = m2)
+
+
+bin_eg = data.table(
+  Low = c(-Inf, 597, 657, 716),
+  High = c(597, 657, 716, Inf)
+)
+
+View(bin_eg)
+
+
+m = glm(
+  default_next_12m ~ 
+    cscore_bin + 
+    ocltv_bin + 
+    dti_bin, 
+  family = binomial,
+  data=all_vars)
 
 DT::datatable(broom::tidy(m), options=list(pageLength=16))
