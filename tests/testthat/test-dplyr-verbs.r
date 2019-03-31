00f2108d371250f59a8cdcd64a33e48fb05f8b69
susabi <zhuojia.dai@gmail.com>
2019-03-31 12:27:55 +1100

    finished testing all dplyr verbs

--- tests/testthat/test-dplyr-verbs.r
+++ tests/testthat/test-dplyr-verbs.r
@@ -5,16 +5,95 @@ setup({
   as.disk.frame(b, "tmp_b_dv.df", nchunks = 5, overwrite = T)
 })
 
-test_that("testing dplyr verbs", {
+test_that("testing select", {
   b = disk.frame("tmp_b_dv.df")
+  
+  df = b %>% 
+    select(a) %>% 
+    collect
+  
+  expect_equal(ncol(df), 1)
 })
 
+test_that("testing rename", {
+  b = disk.frame("tmp_b_dv.df")
+  
+  df = b %>% 
+    rename(a_new_name = a) %>% 
+    collect
+  
+  expect_setequal(colnames(df), c("a_new_name", "b"))
+})
+
+test_that("testing filter", {
+  b = disk.frame("tmp_b_dv.df")
+  
+  df = b %>% 
+    filter(a <= 100, b <= 10) %>% 
+    collect
+  
+  expect_setequal(nrow(df), 10)
+})
+
+test_that("testing mutate", {
+  b = disk.frame("tmp_b_dv.df")
+  
+  df = b %>% 
+    mutate(d = a + b) %>% 
+    collect
+  
+  expect_setequal(sum(df$d), sum(df$a, df$b))
+})
+
+test_that("testing trans_mutate", {
+  b = disk.frame("tmp_b_dv.df")
+  
+  df = b %>% 
+    transmute(d = a + b) %>% 
+    collect
+  
+  expect_setequal(names(df), c("d"))
+})
+
+test_that("testing arrange", {
+  b = disk.frame("tmp_b_dv.df")
+  
+  df = b %>%
+    mutate(random_unif = runif(dplyr::n())) %>% 
+    arrange(desc(random_unif))
+  
+  x = purrr::map_lgl(1:nchunks(df), ~{
+    is.unsorted(.x) == FALSE
+  })
+  
+  expect_true(all(x))
+})
+
+test_that("testing summarise", {
+  b = disk.frame("tmp_b_dv.df")
+  
+  df = b %>%
+    summarise(suma = sum(a)) %>% 
+    collect %>% 
+    summarise(suma = sum(suma))
+  
+  expect_equal(df$suma, collect(b)$a %>% sum)
+})
+
+test_that("testing do", {
+  b = disk.frame("tmp_b_dv.df")
+  
+  df = b %>%
+    do(mlm = lm(a~b, data = .)) %>% 
+    collect
+  
+  x = purrr::map_chr(df$mlm, ~{
+    class(.x)
+  })
+    
+  expect_true(all(x == "lm"))
+})
 
 teardown({
-  fs::dir_delete("tmp_a_ij.df")
-  fs::dir_delete("tmp_b_ij.df")
-  fs::dir_delete("tmp_d_ij.df")
-  fs::dir_delete("tmp_a_ij2.df")
-  fs::dir_delete("tmp_b_ij2.df")
-  fs::dir_delete("tmp_d_ij2.df")
+  fs::dir_delete("tmp_d_dv.df")
 })
