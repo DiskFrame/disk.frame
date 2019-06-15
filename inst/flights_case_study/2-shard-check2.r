c23483a30786f8758859a2b53907eab743c0e587
ZJ <zhuojia.dai@gmail.com>
2019-06-16 00:54:07 +1000

    minor fixes to flights case study

--- inst/flights_case_study/2-shard-check2.r
+++ inst/flights_case_study/2-shard-check2.r
@@ -1,24 +1,24 @@
-library(disk.frame)
-plan(multiprocess)
-library(magrittr)
-
-a = disk.frame("flights_all_sharded.df")
-b = disk.frame("flights_all_notsharded.df")
-
-a2 = a %>% 
-  group_by(UniqueCarrier) %>% 
-  summarise(n=n()) %>% 
-  collect %>% 
-  group_by(UniqueCarrier) %>% 
-  summarise(n=sum(n)) %>% 
-  arrange(UniqueCarrier)
-
-b2 = b %>% 
-  group_by(UniqueCarrier) %>% 
-  summarise(n=n()) %>% 
-  collect %>% 
-  group_by(UniqueCarrier) %>% 
-  summarise(n=sum(n)) %>% 
-  arrange(UniqueCarrier)
-
-identical(a2, b2)
+  library(disk.frame)
+  plan(multiprocess)
+  library(magrittr)
+  
+  a = disk.frame("flights_all_sharded.df")
+  b = disk.frame("flights_all_notsharded.df")
+  
+  a2 = a %>% 
+    group_by(UniqueCarrier) %>% 
+    summarise(n=n()) %>% 
+    collect %>% 
+    group_by(UniqueCarrier) %>% 
+    summarise(n=sum(n)) %>% 
+    arrange(UniqueCarrier)
+  
+  b2 = b %>% 
+    group_by(UniqueCarrier) %>% 
+    summarise(n=n()) %>% 
+    collect %>% 
+    group_by(UniqueCarrier) %>% 
+    summarise(n=sum(n)) %>% 
+    arrange(UniqueCarrier)
+  
+  identical(a2, b2)
