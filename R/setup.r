ac702963bdf985bce0fb3ecd38a95381d461ee43
evalparse <zhuojia.dai@gmail.com>
2019-07-14 11:52:37 +0300

    Update setup.r

--- R/setup.r
+++ R/setup.r
@@ -1,11 +1,11 @@
 #' Set up disk.frame environment
 #' @param future_backend which future backend to use for parallelisation
-#' @param workers the number of worker (background R processes in the multiprocess environment)
+#' @param workers the number of workers (background R processes in the multiprocess environment)
 #' @param ... passed to `future::plan`
 #' @importFrom future plan multiprocess
 #' @export
 setup_disk.frame <- function(future_backend = multiprocess, workers = parallel::detectCores(logical = F), ...) {    
   future::plan(future_backend, workers = workers, gc = T, ...)
-  options(future.globals.maxSize=Inf)
+  options(future.globals.maxSize=Inf) # do not limit the amount of transfers to other workers
   options(disk.frame.nworkers = workers)
 }
