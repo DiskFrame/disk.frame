9def34c023d4ac905002d04ce8289c0c0a052cc5
ZJ <zhuojia.dai@gmail.com>
2019-07-18 05:55:17 +1000

    minor flights

--- inst/flights_case_study/2-shard-check.r
+++ inst/flights_case_study/2-shard-check.r
@@ -23,3 +23,24 @@ b = list.files("inst/flights_case_study/data",
                                  overwrite=T)) %>% 
   rbindlist.disk.frame(outdir = "flights_all_notsharded.df", by_chunk_id = T)
 
+
+aa = a[,.N, UniqueCarrier, keep=c("UniqueCarrier")][,sum(N), UniqueCarrier][order(UniqueCarrier)]
+
+bb = b[,.N, UniqueCarrier, keep=c("UniqueCarrier")][,sum(N), UniqueCarrier][order(UniqueCarrier)]
+
+identical(aa,bb)
+
+a2 = a %>% 
+  group_by(UniqueCarrier) %>% 
+  summarise(n=n()) %>% 
+  collect %>% 
+  group_by(UniqueCarrier) %>% 
+  summarise(n=sum(n)) %>% 
+  order_by(UniqueCarrier)
+
+b2 = b %>% group_by(UniqueCarrier) %>% 
+  summarise(n=count()) %>% 
+  collect %>% 
+  group_by(UniqueCarrier) %>% 
+  summarise(n=sum(n))
+
--- inst/flights_case_study/2-shard-check2.r
+++ inst/flights_case_study/2-shard-check2.r
@@ -1,24 +1,31 @@
-  library(disk.frame)
-  plan(multiprocess)
-  library(magrittr)
-  
-  a = disk.frame("flights_all_sharded.df")
-  b = disk.frame("flights_all_notsharded.df")
-  
-  a2 = a %>% 
-    group_by(UniqueCarrier) %>% 
-    summarise(n=n()) %>% 
-    collect %>% 
-    group_by(UniqueCarrier) %>% 
-    summarise(n=sum(n)) %>% 
-    arrange(UniqueCarrier)
-  
-  b2 = b %>% 
-    group_by(UniqueCarrier) %>% 
-    summarise(n=n()) %>% 
-    collect %>% 
-    group_by(UniqueCarrier) %>% 
-    summarise(n=sum(n)) %>% 
-    arrange(UniqueCarrier)
-  
-  identical(a2, b2)
+library(disk.frame)
+plan(multiprocess)
+library(magrittr)
+
+a = disk.frame("flights_all_sharded.df")
+b = disk.frame("flights_all_notsharded.df")
+
+a2 = a %>% 
+  group_by(UniqueCarrier) %>% 
+  summarise(n=n()) %>% 
+  collect %>% 
+  group_by(UniqueCarrier) %>% 
+  summarise(n=sum(n)) %>% 
+  arrange(UniqueCarrier)
+
+b2 = b %>% 
+  group_by(UniqueCarrier) %>% 
+  summarise(n=n()) %>% 
+  collect %>% 
+  group_by(UniqueCarrier) %>% 
+  summarise(n=sum(n)) %>% 
+  arrange(UniqueCarrier)
+
+identical(a2, b2)
+
+fa = list.files("inst/flights_case_study/data", 
+           pattern="*.csv", 
+           full.names = T) %>% 
+  head(10)
+
+ab = csv_to_disk.frame(fa, outdir ="pls_del_tmp", overwrite = T)
