97ec4edcb6d98d63c8d0f2b035bb0553ca55cb50
susabi <zhuojia.dai@gmail.com>
2019-07-23 01:12:04 -0700

    finalised data.table functionality

--- inst/add_chunk_readr.r
+++ inst/add_chunk_readr.r
@@ -1,18 +1,22 @@
+library(disk.frame)
 path = "d:/data/Performance_All/Performance_2016Q2.txt"
 
-library(disk.frame)
 
-delete(df)
 df = disk.frame("pls_del")
-df
-
 system.time(
   a <- readr::read_delim_chunked(
     path, 
     function(x, y) {
       disk.frame::add_chunk(df, x, y)
       y
-      },
-    delim = "|", chunk_size = 1e6) 
+    },
+    delim = "|", chunk_size = 1e6, col_names = F) 
 )
+df
+#delete(df)
+
+y = 2
+
+df[1:2,.N + y, X1]
+
 
--- tests/testthat/test-data-table.r
+++ tests/testthat/test-data-table.r
@@ -1,14 +1,20 @@
 context("test-data.table [")
 
 setup({
-  setup_disk.frame(workers = 1)
-  df = as.disk.frame(disk.frame:::gen_datatable_synthetic(1e5+11), "tmp_col_delete", overwrite=T)
+  setup_disk.frame(workers = 2)
+  df = as.disk.frame(disk.frame:::gen_datatable_synthetic(1e5+11), "tmp_col_delete", overwrite=T, nchunks = 8)
 })
 
 test_that("data.table .N", {
   df = disk.frame("tmp_col_delete")
-  expect_equal(sum(df[,.N]), 1e5+11)
+  expect_equal(sum(unlist(df[,.N])), 1e5+11)
 })
+
+# test_that("data.table .N+y V1", {
+#   df = disk.frame("tmp_col_delete")
+#   y = 2 
+#   expect_equal(sum(unlist(df[,.N + y, v1])), 1e5+11)
+# })
 
 teardown({
   fs::dir_delete("tmp_col_delete")
