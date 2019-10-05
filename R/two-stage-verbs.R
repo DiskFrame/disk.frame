80b2c80ef0dd5be8c6b883d4e8ab3c52b8f4d696
evalparse <zhuojia.dai@gmail.com>
2019-10-05 20:42:59 +1000

    init chunk-group-by

---
+++ R/two-stage-verbs.R
@@ -0,0 +1,21 @@
+#' A two-stage aggregation framework
+#'
+#' @param chunkfn a function to apply to each chunk. The results are passed to a central location for aggregation
+#' @param agg The aggregation function in the central location each chunk
+#' @param finalize A function to run at the end
+#'
+#' @return a disk.frame.agg
+#' @export
+#'
+#' @examples
+#' 
+#' # defining the mean aggregation
+#' 
+#' chunkfn <- function(chunk) {
+#' }
+# learning from https://docs.dask.org/en/latest/dataframe-groupby.html
+group_by_aggregation <- function(chunkfn, agg, finalize) {
+  res = list(chunkfn, agg, finalize)
+  class(res) <- "disk.frame.agg"
+  res
+}
