437af9d168e38ddbc457789f7d8fc7ece80947f1
Jacky Poon <jackypoon0@gmail.com>
2019-10-13 19:40:45 +1100

    Revert "Update tests"
    This reverts commit b6c39a88f56b9209e08020bb350ee0cceeb9000f.

--- R/hard_arrange.r
+++ R/hard_arrange.r
@@ -40,25 +40,17 @@ hard_arrange.data.frame <- function(df, ...) {
 hard_arrange.disk.frame <- function(df, ..., outdir=tempfile("tmp_disk_frame_hard_arrange"), nchunks = disk.frame::nchunks(df), overwrite = TRUE) {
   overwrite_check(outdir, overwrite)
   
-  # Refer also to Dplyr arrange: https://github.com/tidyverse/dplyr/blob/master/src/arrange.cpp
-  q <- enquos(...)
-  is_sym <- sapply(q, rlang::quo_is_symbol)
-  arrange_codes <- sapply(q, rlang::as_label)
+  # Split variables from "desc" components
+  # Limitations: this does not evaluate expressions - e.g. arrange by f(x)
+  q <- quos(...)
   
-  # Check if desc...
-  is_desc <- substr(arrange_codes, 1, 5) == "desc("
+  vars <- sapply(q, function(x){ 
+    expr <- sub(")", "", sub("desc(", "", as.character(x)[2], fixed=TRUE), fixed=TRUE)
+  })
   
-  # If expr is a symbol from the data, just use it.
-  # Otherwise need to evaluate ... 
-  # (TODO - currently only support variables and desc in the data)
-  # Peels off "desc" from the original
-  vars <- sub(")", "", sub("desc(", "", arrange_codes, fixed=TRUE), fixed=TRUE)
-
-  desc_vars <- vars[is_desc]
-  
-  if(!all(vars %in% colnames(df))){
-    stop(paste0("Expressions currently not supported. Columns not found in colnames:", vars[!vars %in% colnames(df)]))
-  }
+  desc_vars <- vars[sapply(q, function(x){ 
+    expr <- grepl("desc(", as.character(x)[2], fixed=TRUE)
+  })]
   
   # Hard group by in a partially sorted way at the chunk level and then arrange within chunks
   df %>% 
