393a8c747cd04ea5cdf440dc37b8d9bb10279d97
Jacky Poon <jackypoon0@gmail.com>
2019-10-13 18:46:45 +1100

    Update test-hard-arrange.R - extend tests
    Extend tests to test up to three factors

--- tests/testthat/test-hard-arrange.R
+++ tests/testthat/test-hard-arrange.R
@@ -3,22 +3,53 @@ context("test-arrange")
 setup({
   
   df = disk.frame:::gen_datatable_synthetic(1e3+11)
-  data.table::fwrite(df, "tmp_pls_delete_gb.csv")
+  data.table::fwrite(df, file.path(tempdir(), "tmp_pls_delete_gb.csv"))
 })
 
 
 test_that("test hard_arrange on disk.frame", {
-  dff = csv_to_disk.frame("tmp_pls_delete_gb.csv", "tmp_pls_delete_gb.df")
+  dff = csv_to_disk.frame(
+    file.path(tempdir(), "tmp_pls_delete_gb.csv"), 
+    file.path(tempdir(), "tmp_pls_delete_gb.df"))
   
-  # Sort ascending
-  sorted_dff <- dff %>% hard_arrange(id1, id4)
+  # Sort ascending, one level
+  sorted_dff <- dff %>% hard_arrange(id1)
   sorted_df <- sorted_dff %>% collect
+  
   expect_true(!is.unsorted(sorted_df$id1))
   
-  # Sort decending
+  # Sort ascending, two levels
+  sorted_dff <- dff %>% hard_arrange(id1, id4)
+  sorted_df <- sorted_dff %>% collect
+  sorted_df$id1_id4 <- paste0(
+    sorted_df$id1, 
+    formatC(sorted_df$id4, width=3, format="d", flag= "0")
+  )
+  expect_true(!is.unsorted(sorted_df$id1_id4))
+  
+  # Sort decending, two levels
   desc_dff <- dff %>% hard_arrange(desc(id4), id2)
   desc_dff <- desc_dff %>% collect
+  
+  #  Level 1
   expect_true(!is.unsorted(-desc_dff$id4))
+  
+  #  Level 2
+  desc_dff$id4_id2 <- paste0(
+    formatC(max(desc_dff$id4) - desc_dff$id4, width=3, format="d", flag= "0"), 
+    desc_dff$id2)
+  expect_true(!is.unsorted(-desc_dff$id4))
+  
+  # Sort ascending, three levels, from already partially sorted disk frame
+  sorted_dff2 <- sorted_dff %>% hard_arrange(id1, id4, id6)
+  sorted_df2 <- sorted_dff2 %>% collect
+
+  sorted_df2$id1_id4_id6 <- paste0(
+    sorted_df2$id1,
+    formatC(sorted_df2$id4, width=3, format="d", flag= "0"),
+    formatC(sorted_df2$id6, width=3, format="d", flag= "0"))
+  expect_true(!is.unsorted(sorted_df2$id1_id4_id6))
+  
 })
 
 test_that("test hard_arrange on data.frame", {
