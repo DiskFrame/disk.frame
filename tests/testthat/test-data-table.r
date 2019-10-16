c3b617aaa9c11d22711d749b2044513f47cd5154
evalparse <zhuojia.dai@gmail.com>
2019-10-17 00:20:35 +1100

    updated some more tests

--- tests/testthat/test-data-table.r
+++ tests/testthat/test-data-table.r
@@ -3,7 +3,7 @@ context("test-data.table [")
 setup({
   library(data.table)
   setup_disk.frame(workers = 2)
-  df = as.disk.frame(disk.frame:::gen_datatable_synthetic(1e5+11), file.path(tempdir(), "tmp_col_delete"), overwrite=T, nchunks = 8)
+  df = as.disk.frame(disk.frame:::gen_datatable_synthetic(1e5+11), file.path(tempdir(), "tmp_col_delete"), overwrite=TRUE, nchunks = 8)
 })
 
 test_that("data.table .N", {
@@ -26,6 +26,14 @@ test_that("data.table .N+y V1", {
     # TODO figure out why the above fails
     expect_equal(2L, 2L)
   }
+})
+
+test_that("data.table do not return a data.table", {
+  library(data.table)
+  df = disk.frame(file.path(tempdir(), "tmp_col_delete"))
+  res = df[,.(.N), rbind=FALSE]
+  expect_equal(typeof(res), "list")
+  expect_equal(length(res), 8)
 })
 
 teardown({
